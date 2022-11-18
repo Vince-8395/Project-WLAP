8033b329666c1bf786532c6558af12fe62dacc62
Vince-8395 <115220055+Vince-8395@users.noreply.github.com>
2022-11-17 17:23:42 -0700

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <li>
+                <Link to="/">
+
+                </Link>
+            </li>
+            <li>
+                <Link to="/">
+
+                </Link>
+            </li>
+        </div>
+    );
+}
+
+export default Navbar;
